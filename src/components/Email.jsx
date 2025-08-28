6c6b1d097f802e0749fce76f0fcbd472355129be
Hans Jakob Håland <Hans.Jakob.Haland@no.experis.com>
2025-08-28 11:51:19 +0200

    Adds emails component for listing emails

---
+++ src/components/Email.jsx
@@ -0,0 +1,11 @@
+import {useState} from "react";
+
+const Email = () => {
+    const [read, setRead] = useState(false);
+    const [starred, setStarred] = useState(false);
+    return (
+        <>
+        
+        </>
+    )
+}
